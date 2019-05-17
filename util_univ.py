f9d82ccfba9a96864c1e9b2b579e9e67cc3c24ef
Takashi Uno <euno.eng@gmail.com>
2019-05-17 11:06:25 +0900

    update fooling rate method

--- util_univ.py
+++ util_univ.py
@@ -23,12 +23,30 @@ def avg_add_clip_pert(avg_img,v):
     return pert_img
 
 
-def target_fooling_rate_calc(v,dataset,f,target):
+def fooling_rate_calc(v,dataset,f,batch_size=100):
+    dataset_perturbed = dataset + v
+    num_images =  np.shape(dataset)[0]
+    est_labels_orig = np.zeros((num_images))
+    est_labels_pert = np.zeros((num_images))
+
+    num_batches = np.int(np.ceil(np.float(num_images) / np.float(batch_size)))
+
+    # Compute the estimated labels in batches
+    for ii in range(0, num_batches):
+        m = (ii * batch_size)
+        M = min((ii+1)*batch_size, num_images)
+        est_labels_orig[m:M] = np.argmax(f(dataset[m:M, :, :, :]), axis=1).flatten()
+        est_labels_pert[m:M] = np.argmax(f(dataset_perturbed[m:M, :, :, :]), axis=1).flatten()
+
+    # Compute the fooling rate
+    fooling_rate = float(np.sum(est_labels_pert != est_labels_orig) / float(num_images))
+    return fooling_rate
+
+def target_fooling_rate_calc(v,dataset,f,target,batch_size=100):
     dataset_perturbed = dataset + v
     num_images =  np.shape(dataset)[0]
     est_labels_pert = np.zeros((num_images))
 
-    batch_size = 100
     num_batches = np.int(np.ceil(np.float(num_images) / np.float(batch_size)))
 
     # Compute the estimated labels in batches
