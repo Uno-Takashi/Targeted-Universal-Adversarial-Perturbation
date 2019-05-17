ebe3fa35857257e4f8070882f06c16679cc9166c
Takashi Uno <euno.eng@gmail.com>
2019-05-17 12:26:00 +0900

    update calc foolingrate mothod

--- util_univ.py
+++ util_univ.py
@@ -59,3 +59,23 @@ def target_fooling_rate_calc(v,dataset,f,target,batch_size=100):
 
     target_fooling_rate = float(np.sum(est_labels_pert == target) / float(num_images))
     return target_fooling_rate
+
+def fooling_rate_calc_all(v,dataset,f,target,batch_size=100):
+    dataset_perturbed = dataset + v
+    num_images =  np.shape(dataset)[0]
+    est_labels_orig = np.zeros((num_images))
+    est_labels_pert = np.zeros((num_images))
+
+    num_batches = np.int(np.ceil(np.float(num_images) / np.float(batch_size)))
+
+    # Compute the estimated labels in batches
+    for ii in range(0, num_batches):
+        m = (ii * batch_size)
+        M = min((ii+1)*batch_size, num_images)
+        est_labels_orig[m:M] = np.argmax(f(dataset[m:M, :, :, :]), axis=1).flatten()
+        est_labels_pert[m:M] = np.argmax(f(dataset_perturbed[m:M, :, :, :]), axis=1).flatten()
+
+    # Compute the fooling rate
+    fooling_rate = float(np.sum(est_labels_pert != est_labels_orig) / float(num_images))
+    target_fooling_rate = float(np.sum(est_labels_pert == target) / float(num_images))
+    return fooling_rate,target_fooling_rate
